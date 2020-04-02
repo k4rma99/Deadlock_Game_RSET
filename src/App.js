54d4e09cf18252f2b07672c17a77bd314da6e437
k4rma <k4.rma321@gmail.com>
2020-04-02 11:18:40 +0530

    Added button to App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {Navbar} from "react-bootstrap"
-// import Background from "../src/images/backgroundImage"
+import {Navbar, Container} from "react-bootstrap"
 import BackgroundImagePage from "./components/bgImage"
 import "./assets/css/heading-flicker.css"
 import Button from "./components/playGame"
@@ -9,41 +8,13 @@ function App() {
   return (
     <div className="App">
       <BackgroundImagePage />
-      <h1 data-heading="a" id = "heading">deadlock</h1> 
-      <Button />
+      <section className = "head-wrapper" style = {{height : 0}}>
+        <h1 data-heading="a" id = "heading">deadlock</h1> 
+        <Button />
+      </section>
     </div>
   );
 }
 
-class Nav extends React.Component{
-  
-    constructor(){
-      super()
-      this.state = {
-        style : {
-          backgroundColor : "#180c4d"
-        }
-      }
-    }
-
-  render(){
-    return(
-      <div id = "navbar-wrapper" style = {this.state.style}>
-        <Navbar bg = "light">
-        <Navbar.Brand href="#home">
-        <img
-          src="/logo.svg"
-          width="30"
-          height="30"
-          className="d-inline-block align-top"
-          alt="React Bootstrap logo"
-        />
-        </Navbar.Brand>
-        </Navbar>
-      </div>
-    )
-  }
-
-}
 
 export default App;
--- src/components/playGame.js
+++ src/components/playGame.js
@@ -1,27 +1,15 @@
 import React from "react"
 import { AlertHeading } from "react-bootstrap/Alert"
-
-
+import "../assets/css/playGameButton.css"
+import Svg from "./playGameSVG"
 class playGameButton extends React.Component{
     render(){
-
-        let style = {
-            fontSize: "3vw",
-            textAlign: "center",
-            lineHeight: 1,
-            top: "22vw",
-            left: "39vw",
-            position: "absolute",
-            color: "#2d2d2d",
-            letterSpacing: "-.1rem"
-        }
-        
-
         return(
-            <div style = {style}>
-                <a href="http://google.com" class="btn-liquid">
-                    <span class="inner">Liquid button ?</span>
-                </a>
+            <div class="wrapper">
+            <a class="cta" href="#">
+                <span>PLAY GAME</span>
+                <Svg />
+            </a>
             </div>
         )
     }
