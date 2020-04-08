b348ca4e38c05e6a43a4f7c4a414b94d90884240
Jacob1248 <jacobchacko228@gmail.com>
2020-04-09 00:23:23 +0530

    Completely fixed all alignment issues

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import { ScrollSnap } from './components/ScrollSnap.jsx';
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <AnimatedBackground></AnimatedBackground>
       <ScrollSnap></ScrollSnap>
       </div>
